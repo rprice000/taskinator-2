723f007a8f2429a0e13a77026607c0438e151feb
rprice000 <rtprice21@gmail.com>
2022-03-01 10:51:35 -0600

    finished 4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Line below stores the button DOM object to a variable
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+// Line below stores the form DOM object to a variable
+var formEl = document.querySelector("#task-form");
 // Line below stores the ul DOM object to a variable
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -16,7 +18,11 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // });
 
 // Creates a named function for creating a new task
-var createTaskHandler = function() {
+// added the event object to the function as an arugment to hold what should happen in place of the refreshing the browser
+var createTaskHandler = function(event) {
+// Without line below every time task is submitted the page will refesh and clear the unordered list.  Event.preventdefault stops this from happening
+    event.preventDefault();
+    console.log(event);
 // // Line below creates a new list item  
     var listItemEl = document.createElement("li");
 // // Line below adds a class to the new list item
@@ -28,4 +34,6 @@ var createTaskHandler = function() {
   }
 
 // Line below selects the button DOM object variable uses the addEventListener method and the passes the argument click then runs the createTaskHandler function
-buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", createTaskHandler);
+// Line below selects the form DOM object variable uses the addEventListener method and the passes the argument submit then runs the createTaskHandler function
+formEl.addEventListener("submit", createTaskHandler);
