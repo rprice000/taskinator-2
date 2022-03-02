5827947632dace6f3f26164f32885c0bbc31ab0d
rprice000 <rtprice21@gmail.com>
2022-03-02 13:29:39 -0600

    finished 4.2.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,15 +22,29 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 // Without line below every time task is submitted the page will refesh and clear the unordered list.  Event.preventdefault stops this from happening
     event.preventDefault();
-    console.log(event);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
 // // Line below creates a new list item  
     var listItemEl = document.createElement("li");
 // // Line below adds a class to the new list item
     listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
 // // Line below adds text content to the new list item
-    listItemEl.textContent = "This is a new task.";
+    // listItemEl.textContent = taskNameInput;
+    // adds the taskInfoEl to the listItemEl as a child
+    listItemEl.appendChild(taskInfoEl);
 // // Line below adds the new list item to the unordered list object
     tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
   }
 
 // Line below selects the button DOM object variable uses the addEventListener method and the passes the argument click then runs the createTaskHandler function
